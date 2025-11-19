97d4ed6e284a8f9c37b094bdbb94bf797a815d4f
Skye Harris <skyeserenaharris@gmail.com>
2025-11-19 19:10:14 +0800

    Bugfix: return an empty array if this has not been set, such as with AI Task entities

--- custom_components/local_openai/conversation.py
+++ custom_components/local_openai/conversation.py
@@ -60,7 +60,7 @@ class LocalAiConversationEntity(LocalAiEntity, conversation.ConversationEntity):
         hass_apis = [api.id for api in llm.async_get_apis(self.hass)]
 
         # Filter out any tool providers that no longer exist
-        llm_apis = options.get(CONF_LLM_HASS_API)
+        llm_apis = options.get(CONF_LLM_HASS_API, [])
         llm_apis = [api for api in llm_apis if api in hass_apis]
 
         try:
